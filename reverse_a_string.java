fef9a0e610fe65e5964c5a1300340ecb0b510ddc
googalvel <59963421+googalvel@users.noreply.github.com>
2022-08-08 11:22:11 +0530

    first commit

--- reverse_a_string.java
+++ reverse_a_string.java
@@ -1,15 +1,18 @@
-public class cfg{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        String str=sc.nextLine();
+import java.util.Scanner;
 
+public class reverse_a_string{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String str=sc.nextLine();
         String rstr="";
         char ch;
 
-        for(i=0;i<str.length();i++){
-            ch=charAt(i);
-            nstr=ch+nstr;
+        for(int i=0;i<str.length();i++){
+            ch=str.charAt(i);
+            rstr=ch+rstr;
         }
-        System.out.println("reverse string: " + nstr);
+        System.out.println("reverse string: " + rstr);
     }
+
 }
